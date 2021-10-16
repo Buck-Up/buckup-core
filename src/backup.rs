da0d1908c0d76235e51dd0fc54c53a248fa831b9
Russell Cousineau <miller.time.baby@gmail.com>
2021-10-15 22:22:46 -0700

    add Display impl for Backup

--- src/backup.rs
+++ src/backup.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::{fmt, path::PathBuf};
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Backup {
@@ -19,5 +19,23 @@ impl Backup {
 
     pub fn add_source(&mut self, source: PathBuf) {
         self.sources.push(source);
+    }
+}
+
+impl fmt::Display for Backup {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let sources = self
+            .sources
+            .iter()
+            .map(|s| format!("- {:?}", s))
+            .collect::<Vec<String>>()
+            .join("\n");
+        let descriptions = vec![
+            format!("Name: {}", self.name),
+            format!("Sources:\n{}", sources),
+            format!("Dest: {:?}", self.dest),
+        ]
+        .join("\n");
+        write!(f, "{}", descriptions)
     }
 }
