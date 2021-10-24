52e6832a47a1bf4fe607be69c9839f64fd33015e
Russell <miller.time.baby@gmail.com>
2021-10-23 19:48:19 -0700

    add utils for saving/loading config file

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,13 @@
 use crate::Backup;
 use serde::{Deserialize, Serialize};
+use std::{
+    env,
+    error::Error,
+    fs,
+    path::{Path, PathBuf},
+};
+
+const CONFIG_FILENAME: &str = ".strongbox.toml";
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Config {
@@ -10,4 +18,37 @@ impl Config {
     pub fn add_backup(&mut self, backup: Backup) {
         self.backups.push(backup);
     }
+
+}
+
+pub fn initialize_config() -> Result<(), Box<dyn Error>> {
+    let config = load_config()?;
+    save_config(&config)?;
+    let contents = toml::to_string_pretty(&config)?;
+    println!("config initialized at {:?}", config_path());
+    println!("{}", contents);
+    Ok(())
+}
+
+pub fn load_config() -> Result<Config, Box<dyn Error>> {
+    let f = config_path();
+    if f.exists() {
+        let contents = fs::read_to_string(f)?;
+        let config = toml::from_str(&contents)?;
+        Ok(config)
+    } else {
+        Ok(Config::default())
+    }
+}
+
+pub fn save_config(config: &Config) -> Result<(), Box<dyn Error>> {
+    let f = config_path();
+    let contents = toml::to_string_pretty(config)?;
+    fs::write(f, contents)?;
+    Ok(())
+}
+
+fn config_path() -> PathBuf {
+    let home_dir = env::var("HOME").unwrap();
+    Path::new(&home_dir).join(CONFIG_FILENAME)
 }
