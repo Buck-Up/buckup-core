cb703010b5abb185d9702572cff3f61a3ee07328
Russell Cousineau <miller.time.baby@gmail.com>
2022-01-29 18:32:34 -0800

    add file sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod backup;
 pub mod config;
+pub mod file_sync;
 pub mod registry;
 pub(crate) mod util;
 
 pub use backup::Backup;
 pub use config::Config;
+pub use file_sync::FileSync;
 pub use registry::Registry;
